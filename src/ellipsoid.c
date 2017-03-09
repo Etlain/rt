43cbd1b6307226dfa6178832b10783af7e3056de
Mounir MOUHSSINE <mmouhssi@e1r8p14.42.fr>
2017-03-09 12:05:32 +0100

    norme ellipsoid

--- src/ellipsoid.c
+++ src/ellipsoid.c
@@ -6,18 +6,18 @@
 /*   By: mmouhssi <mmouhssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 16:52:08 by mmouhssi          #+#    #+#             */
-/*   Updated: 2017/03/09 11:32:24 by mmouhssi         ###   ########.fr       */
+/*   Updated: 2017/03/09 12:04:42 by mmouhssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/rt.h"
 
-double check_ellipsoid(t_ray *ray, t_object obj)
+double		check_ellipsoid(t_ray *ray, t_object obj)
 {
 	double *pos;
 	double *res;
 	double *p;
-    double a;
+	double a;
 
 	p = (double*)malloc(sizeof(double) * 3);
 	res = (double*)malloc(sizeof(double) * 2);
@@ -26,14 +26,14 @@ double check_ellipsoid(t_ray *ray, t_object obj)
 	pos[1] = ray->a.y - obj.a.y;
 	pos[2] = ray->a.z - obj.a.z;
 	p[0] = (ray->dir.x * ray->dir.x) / (obj.b.x * obj.b.x)
-    + (ray->dir.y * ray->dir.y) / (obj.b.y * obj.b.y)
+	+ (ray->dir.y * ray->dir.y) / (obj.b.y * obj.b.y)
 	+ (ray->dir.z * ray->dir.z) / (obj.b.z * obj.b.z);
 	p[1] = 2 * ((ray->dir.x * pos[0]) / (obj.b.x * obj.b.x)
-    + (ray->dir.y * pos[1]) / (obj.b.y * obj.b.y)
-    + (ray->dir.z * pos[2]) / (obj.b.z * obj.b.z));
+	+ (ray->dir.y * pos[1]) / (obj.b.y * obj.b.y)
+	+ (ray->dir.z * pos[2]) / (obj.b.z * obj.b.z));
 	p[2] = (pos[0] * pos[0]) / (obj.b.x * obj.b.x)
-    + (pos[1] * pos[1]) / (obj.b.y * obj.b.y)
-    + (pos[2] * pos[2]) / (obj.b.z * obj.b.z) - 1;
+	+ (pos[1] * pos[1]) / (obj.b.y * obj.b.y)
+	+ (pos[2] * pos[2]) / (obj.b.z * obj.b.z) - 1;
 	a = ft_condition(res, degree_2(p, res));
 	free(pos);
 	free(p);
@@ -41,10 +41,10 @@ double check_ellipsoid(t_ray *ray, t_object obj)
 	return (a);
 }
 
-t_v 		normal_ellipsoid(t_ray *ray, t_object *obj)
+t_v			normal_ellipsoid(t_ray *ray, t_object *obj)
 {
 	t_v		res;
-	double *pos;
+	double	*pos;
 
 	pos = (double*)malloc(sizeof(double) * 3);
 	pos[0] = ray->a.x - obj->a.x;
